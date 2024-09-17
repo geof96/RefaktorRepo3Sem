eb61f9f233e1531324103319e49cc1ed5f9a2120
geof96 <leyton2730@gmail.com>
2024-09-17 10:19:26 +0200

    Fixed the wrong input, for better error handling. Now it wont loop the message, nor show a inputmismatch exception.

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,12 +3,14 @@ import java.util.Scanner;
 
 public class Calculator {
     public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
-// "char" kan bruges ligesom string og int. Det må dog kun være et tegn.
+
         double num1 = 0;
         double num2 = 0;
         char op;
         double result;
+
 
         while (true) {
             System.out.println("Velkommen til regnemaskinen");
@@ -21,24 +23,25 @@ public class Calculator {
 
             if (op == 'q') break;
             num1 = inputDouble("write first number : ");
-
-
-            System.out.print("Write second number : ");
-            num2 = input.nextDouble();
+            num2 = inputDouble2("write first number : ");
 
             switch (op) {
                 case '+':
                     result = num1 + num2;
                     break;
+
                 case '-':
                     result = num1 - num2;
                     break;
+
                 case '*':
                     result = num1 * num2;
                     break;
+
                 case '/':
                     result = num1 / num2;
                     break;
+
                 default:
                     System.out.print("ugyldig oprator (regne tegn)");
                     input.nextLine();
@@ -52,7 +55,7 @@ public class Calculator {
 
     static double inputDouble(String promt) {
         Scanner input = new Scanner(System.in);
-        boolean ok = false;
+        boolean ok;
         double num = 0;
 
         do {
@@ -62,8 +65,26 @@ public class Calculator {
                 ok = true;
             } catch (InputMismatchException e) {
                 System.out.println("wrong input, try again");
+                ok = true;
             }
         } while (!ok);
         return num;
+    }
+    static double inputDouble2(String promt) {
+        Scanner input = new Scanner(System.in);
+        boolean ok;
+        double num2 = 0;
+
+        do {
+            try {
+                System.out.print(promt);
+                num2 = input.nextDouble();
+                ok = true;
+            } catch (InputMismatchException e) {
+                System.out.println("wrong input, try again");
+                ok = true;
+            }
+        } while (!ok);
+        return num2;
     }
 }
